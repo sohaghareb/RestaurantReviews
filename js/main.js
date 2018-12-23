e334e8a498e7ce3b4fce490d923cbfb34e345f6b
Soha GHARIB <soha.gharib@valeo.com>
2018-12-23 11:00:22 +0200

    added SW

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('js/sw.js').then(function (registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
   initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
