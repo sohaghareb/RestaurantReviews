64618780db8ae880bd3a04b11cb2216120fa67ab
Soha GHARIB <soha.gharib@valeo.com>
2018-12-23 14:58:06 +0200

    finished with SW and responsive design

--- js/sw.js
+++ js/sw.js
@@ -1,14 +1,12 @@
-console.log('I am a service worker');
-var CACHE_NAME = 'my-cache';
+let CACHE_NAME = 'restaurant-cache';
 var urlsToCache = [
     '/',
     '/css/styles.css',
     '/js/main.js',
     '/js/restaurant_info.js'
 ];
-
+// First I need to install Service worker
 self.addEventListener('install', function (event) {
-    // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
@@ -17,3 +15,46 @@ self.addEventListener('install', function (event) {
             })
     );
 });
+
+//Secondly, I need to activate the SW (Mainly clean up old SW jobs)
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function (cacheNames) {
+                return Promise.all(
+                    cacheNames.filter(function (cacheName) {
+                        return cacheName.startsWith('restaurant-') &&
+                            cacheName != CACHE_NAME;
+                    }).map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+    );
+});
+
+//Cache all fetch requests 
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+            console.log('No response found in cache. About to fetch from network...');
+
+            return fetch(event.request).then(function (response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function (error) {
+                console.error('Fetching failed:', error);
+
+                throw error;
+            });
+        })
+    );
+}); 
